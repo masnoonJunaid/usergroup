d4440b04f5d556b01ef7d7ab1a96b57ae45bc450
Masnoon Junaid <masnoon20@gmail.com>
2020-12-13 19:17:05 +0530

    components structure

---
+++ src/components/CreateGroup.js
@@ -0,0 +1,9 @@
+function CreateGroup(){
+  return (
+    <div>
+      <h1>Hello from CreateGroup</h1>
+    </div>
+  )
+}
+
+export default CreateGroup
---
+++ src/components/UserCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class UserCard extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1>Hello from UserCard</h1>
+      </div>
+    )
+  }
+
+}
+
+export default UserCard
